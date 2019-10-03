2e112553d870fcbc90d79680e82696a3cf089f51
Chilos Ohh <chilos.ohh@yandex.ru>
2019-10-03 19:00:51 +0300

    Добавил миграцию

---
+++ Sparta.Web/Migrations/20191003160011_changePassword.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sparta.Web.Migrations
+{
+    public partial class changePassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NeedChangePassword",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NeedChangePassword",
+                table: "User");
+        }
+    }
+}
