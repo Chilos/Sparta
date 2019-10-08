93bef962c5ee81a258d8abcd7e55aca1cc4f821b
Chilos Ohh <chilos.ohh@yandex.ru>
2019-10-08 18:36:00 +0300

    Добавили обновление пользователей

---
+++ Sparta.Web/API/ViewModel/EditUserViewModal.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sparta.Web.API.ViewModel
+{
+    public class EditUserViewModal : UserViewModal
+    {
+        public string Id { get; set; }
+        public bool IsDropPassword { get; set; }
+    }
+}
--- Sparta.Web/Controllers/UserController.cs
+++ Sparta.Web/Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sparta.Web.API.Services.Abstract;
 using Sparta.Web.API.ViewModel;
 using Sparta.Web.Data.Abstract;
 using Sparta.Web.Model;
@@ -15,10 +17,12 @@ namespace Sparta.Web.Controllers
     {
 
         private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
 
-        public UserController(IUserRepository userRepository)
+        public UserController(IUserRepository userRepository, IAuthService authService)
         {
             _userRepository = userRepository;
+            _authService = authService;
         }
 
         [HttpGet("{id}")]
@@ -34,12 +38,39 @@ namespace Sparta.Web.Controllers
             };
         }
 
+        [HttpPost("update")]
+        public ActionResult<UserInfo> UpdateUser(EditUserViewModal model)
+        {
+            var usernameUniq = _userRepository.FindBy(u=>u.Username == model.Username).Count() == 1;
+            if (!usernameUniq)
+                return BadRequest(new { username = "user with this name already exists" });
+
+            var user = _userRepository.GetSingle(model.Id);
+            user.Username = model.Username;
+            user.PhoneNumber = model.PhoneNumber;
+            if (model.IsDropPassword)
+                user.Password = _authService.HashPassword("password");
+            user.Role = model.Role;
+            user.NeedChangePassword = model.IsDropPassword;
+
+            _userRepository.Update(user);
+            _userRepository.Commit();
+
+            return new UserInfo
+            {
+                Username = user.Username,
+                Id = user.Id,
+                Role = user.Role,
+                PhoneNumber = user.PhoneNumber
+            };
+        }
+
         [HttpGet("users")]
         public ActionResult<UserInfo[]> GetUsers()
         {
             return _userRepository.GetAll().Select(u => new UserInfo
-                {Username = u.Username, Id = u.Id, PhoneNumber = u.PhoneNumber, Role = u.Role}).ToArray();
-            
+            { Username = u.Username, Id = u.Id, PhoneNumber = u.PhoneNumber, Role = u.Role }).ToArray();
+
         }
     }
 }
