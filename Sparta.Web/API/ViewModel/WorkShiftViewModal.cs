5d4a771638cf17bf42108c22261d9828f85a01b7
Chilos Ohh <chilos.ohh@yandex.ru>
2019-10-10 14:50:08 +0300

    Функционал настройки смен

--- Sparta.Web/API/ViewModel/WorkShiftViewModal.cs
+++ Sparta.Web/API/ViewModel/WorkShiftViewModal.cs
@@ -9,13 +9,13 @@ namespace Sparta.Web.API.ViewModel
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public DateTime BeginTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public WorkDaysPeriod WorkDaysPeriod { get; set; }
+        public long BeginTime { get; set; }
+        public long EndTime { get; set; }
+        public int WorkDaysPeriod { get; set; }
         public string Role { get; set; }
 
-        public static WorkShiftViewModal Factory(string id, string name, DateTime beginTime, DateTime endTime,
-            WorkDaysPeriod workDaysPeriod, string role)
+        public static WorkShiftViewModal Factory(string id, string name, long beginTime, long endTime,
+            int workDaysPeriod, string role)
         {
             return new WorkShiftViewModal
             {
